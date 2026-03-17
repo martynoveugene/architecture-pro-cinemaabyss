7027fb58a01a449be2b4409bec931f00a7b678de
Eugene M <martynov.eugene@gmail.com>
2026-03-17 23:38:16 +0300

    proxy java app

---
+++ src/microservices/proxy/src/main/java/com/cinemaabyss/ProxyApplication.java
@@ -0,0 +1,19 @@
+package com.cinemaabyss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+
+
+@SpringBootApplication
+public class ProxyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyApplication.class, args);
+    }
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder().build();
+    }
+}
